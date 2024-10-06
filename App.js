4761e4e7fdf69eef1156b63b1a7c3a0b1377e1df
faiz <faiz@attentive.ai>
2024-10-07 02:08:48 +0530

    Ignited App

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-let str = 'Hello from React';
-let str1 = [];
-for (let index = 0; index < 10; index++) {
-    let text = 'I am child ' + (index+1);
-    let tag = React.createElement('h3', { id: 'testing' }, text);
-    str1.push(tag);
- };
-let content;
-content = [[React.createElement('h1', { id: 'testing' }, 'I am Father 1'), ...str1], [React.createElement('h1', { id: 'testing' }, 'I am Father 2'), ...str1]];
-const Heading = React.createElement('div', {id:'testing'},content);
-const root = ReactDOM.createRoot(document.getElementById('root'));
-// const root1 = ReactDOM.createRoot(document.getElementById('root1'));
-// console.log('Here',Heading);
-root.render(Heading);
-ReactDOM.render(Heading, document.getElementById('root1'));
+const Heading = "Hello, React!";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>{Heading}</h1>);
